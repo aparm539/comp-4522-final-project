87b3c9a865b32dec46b944f40fc715df372e3cab
Sunny Parmar <113143064+aparm539@users.noreply.github.com>
2025-03-18 21:09:58 -0600

    Create containers page.

---
+++ resources/views/containers.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:heading> Lab B220 </x-slot:heading>
+    @foreach($containers as $container)
+        <div class="block border-2 border-gray-300 rounded-md p-4 m-4">
+            <h1> Name: {{$container['chemical_name']}}</h1>
+            <h1> Amount: {{$container['quantity']}}</h1>
+            <h1> Shelf: {{$container->location->location_json['shelf']}}</h1>
+        </div>
+    @endforeach
+    <div>
+        {{$containers->links()}}
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Container;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("home");
+});
+
+Route::get('/containers', function () {
+    $containers = Container::with('location')->paginate(5);
+    return view("containers", ['containers' => $containers]);
 });
