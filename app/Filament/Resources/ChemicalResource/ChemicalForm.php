3968e449c813c44a114490ae74d884c855b43708
Sunny <aparm539@mtroyal.ca>
2025-10-31 10:46:03 -0600

    Refactor ChemicalForm layout to make better use of space

--- app/Filament/Resources/ChemicalResource/ChemicalForm.php
+++ app/Filament/Resources/ChemicalResource/ChemicalForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\ChemicalResource;
 
+use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 
@@ -10,13 +11,19 @@ class ChemicalForm
     public static function make(): array
     {
         return [
-            TextInput::make('cas')
-                ->label('CAS #')
-                ->required(),
-            TextInput::make('name')->required(),
+            Grid::make(2)
+                ->schema([
+                    TextInput::make('cas')
+                        ->label('CAS #')
+                        ->required(),
+                    TextInput::make('name')
+                        ->required(),
+                ]),
             \App\Livewire\HazardClassSelect::make('whmisHazardClasses')
+                ->label('WHMIS Hazard Classes')
                 ->relationship('whmisHazardClasses', 'id')
-                ->required(),
+                ->required()
+                ->columnSpanFull(),
         ];
     }
 }
